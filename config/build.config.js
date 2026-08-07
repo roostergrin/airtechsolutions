b5834e8edd2fcbcd4f53b1dac5ab558cb44a1a90
Tim Dobranski <timdobranski@gmail.com>
2026-08-07 08:28:01 -0700

    requests makes home hero more response to smaller mobile layouts

--- config/build.config.js
+++ config/build.config.js
@@ -21,7 +21,14 @@ export default {
     config.module.rules.push({
       test: /\.svg$/,
       loader: 'svg-inline-loader',
-      exclude: /node_modules/
+      exclude: /node_modules/,
+      options: {
+        // svg-inline-loader strips width/height off the root <svg> by default,
+        // leaving only a viewBox. Safari then cannot derive the missing
+        // dimension when CSS sizes one axis, so logos expanded to fill their
+        // flex line. CSS width/height still override these attributes.
+        removeSVGTagAttrs: false
+      }
     })
   }
 }
